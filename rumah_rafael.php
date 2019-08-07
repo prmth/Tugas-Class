76d037e9a39cb50fc50a971ff6b44152098f0ff9
= <=>
2019-08-07 12:40:00 +0700

    Tambahan pakaian wanita fenny

--- rumah_rafael.php
+++ rumah_rafael.php
@@ -56,3 +56,75 @@ $e -> Setmodel('Tradisional');
 $e -> Setwarna('hijau');
 $e -> Setalamat('bangsri');
 $e -> cetak( );
+
+class PakaianWanita{
+
+	var $Model;
+	var $Warna;
+	var $Merk;
+
+	public function SetModel($Model = ' '){
+		$this ->Model = $Model ;
+	}
+
+	public function SetWarna ($Warna = ' '){
+		$this ->Warna = $Warna ;
+	}
+
+	public function SetMerk ($Merk = ' '){
+		$this ->Merk = $Merk ;
+	}
+
+	public function cetak ( ){
+		echo 'Model:'.$this->Model.'<br>';
+		echo 'Warna:'.$this->Warna.'<br>';
+		echo 'Merk:'.$this->Merk.'<br>';
+	}
+}
+$A  = new PakaianWanita ( );
+$A ->setModel('Hodie');
+$A -> SetWarna('Kuning');
+$A -> SetMerk('Supreme');
+$A -> cetak( );
+
+echo '<br>';
+
+$B  = new PakaianWanita ( );
+$B -> SetModel('Sweater');
+$B -> SetWarna('Hijau');
+$B -> SetMerk('Gucci');
+$B -> cetak( );
+
+echo '<br>';
+
+$C  = new PakaianWanita ( );
+$C -> SetModel('Blazer');
+$C -> SetWarna('Coklat');
+$C -> SetMerk('Flava');
+$C -> cetak( );
+
+echo '<br>';
+
+$D = new PakaianWanita ( );
+$D -> SetModel('Tunik');
+$D -> SetWarna('Biru');
+$D -> SetMerk('Erere Product');
+$D -> cetak( );
+
+echo '<br>';
+
+
+$D = new PakaianWanita ( );
+$D -> SetModel('Blues');
+$D -> SetWarna('Putih');
+$D -> SetMerk('Ahha');
+$D -> cetak( );
+
+
+
+
+
+
+
+
+
